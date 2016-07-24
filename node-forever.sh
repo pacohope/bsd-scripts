8615dbafde95b9e6c827f4ead5a8b962202f874c
Paco Hope <github@filter.paco.to>
2016-07-24 10:49:36 +0100

    Functionally working. Now I need to document it.

--- node-forever.sh
+++ node-forever.sh
@@ -1,50 +1,132 @@
 #!/bin/sh
 
 # PROVIDE: forever
-# REQUIRE: NETWORKING SERVERS DAEMON
-# BEFORE:  LOGIN
-# KEYWORD: shutdown
+
+# To use:
+#  1. Copy this script to /usr/local/etc/rc.d
+#  2. Add lines to /etc/rc.conf or /etc/rc.conf.local
+#     a. forever_enable="YES"
+#     b. forever_script="/path/to/your/script"
+#  3. Optionally: add a few other config options to rc.conf
+#     a. forever_user
+#     b. forever_log="/place/where/you/want/logs"
+#     c. forever_err="/place/where/you/want/errors"
+#     d. forever_flags="additional flags to forever"
+#     e. forever_forever="/path/to/forever/script" (Default: /usr/local/bin/forever)
+#     f. forever_nodeenv="PRODUCTION"
 
 # Taken from http://habrahabr.ru/post/137857/
+# Taken from https://gist.github.com/jellea/6510897
 
 . /etc/rc.subr
 
 name="forever"
-forever="/usr/local/bin/node /usr/local/bin/forever"
-workdir="/usr/local/www"
-script="web.js"
-
-rcvar=forever_enable
+rcvar="forever_enable"
 extra_commands="status"
 
 start_cmd="start"
 status_cmd="status"
 stop_cmd="stop"
 restart_cmd="restart"
+start_precmd="${name}_precmd"
 
 load_rc_config $name
-eval "${rcvar}=\${${rcvar}:-'NO'}"
+# Default values
+: ${forever_enable:="NO"}
+: ${forever_user:="www"}
+: ${forever_root:="/var/run"}
+: ${forever_sourcedir:="/usr/local/lib/node_modules"}
+: ${forever_workingdir:="/usr/local/lib/node_modules"}
+: ${forever_forever:="/usr/local/bin/forever"}
+: ${forever_flags:="-a"}
+: ${forever_script:=""}
+: ${forever_scriptargs:=""}
+: ${forever_max:=""}
+: ${forever_logfile:="/var/log/forever.log"}
+: ${forever_outfile:="/var/log/forever-out.log"}
+: ${forever_errfile:="/var/log/forever-err.log"}
+: ${forever_path:=""}
+: ${forever_command:=""}
+: ${forever_pid:="/var/run/forever.pid"}
+: ${forever_nodeenv:="PRODUCTION"}
 
-HOME=/var/run/forever
+export NODE_ENV="${forever_nodeenv}"
+export FOREVER_ROOT="${forever_root}"
+
 start()
 {
-  NODE_ENV=production
-  su -m www -c "exec ${forever} start -a -l /var/log/forever.log -o /dev/null -e /var/log/node_err.log -p /var/run/forever ${script}"
+  ARGS="${forever_flags} -l ${forever_logfile} -o ${forever_outfile} -e ${forever_errfile} -p ${forever_pid}"
+  if [ "${forever_max}" != "" ]
+  then
+    ARGS="${ARGS} -m ${forever_max}"
+  fi
+
+  if [ "${forever_sourcedir}" != "" ]
+  then
+    ARGS="${ARGS} --sourceDir ${forever_sourcedir}"
+  fi
+
+  /usr/bin/su -m "${forever_user}" -c \
+    "${forever_forever} start ${ARGS} ${forever_script} ${forever_scriptargs}"
 }
 
 status()
 {
-  su -m www -c "exec ${forever} list"
+  /usr/bin/su -m "${forever_user}" -c "${forever_forever} list"
 }
 
 stop()
 {
-  su -m www -c "exec ${forever} stop ${script}"
+  /usr/bin/su -m "${forever_user}" -c "${forever_forever} stop ${forever_script}"
 }
 
 restart()
 {
-  su -m www -c "exec ${forever} restart ${script}"
+  /usr/bin/su -m "${forever_user}" -c "${forever_forever} restart ${forever_script}"
+}
+
+
+# Make sure the script file is defined and is readable.
+# Bail otherwise
+forever_precmd() {
+  if [ "${forever_script}" = "" ]
+  then
+    err "forever_script is undefined"
+  fi
+
+  if [ ! -r "${forever_sourcedir}/${forever_script}" ]
+  then
+    err 5 "\"${forever_script}\" is not readable"
+  fi
+
+  # Only root can write log files and pid files to these locations. So we
+  # pre-create the files and chown them to the right user. I have no idea
+  # if that's the right convention or not.
+  [ "${forever_logfile}" != "" ] && \
+    /usr/bin/touch "${forever_logfile}" && \
+    /usr/sbin/chown "${forever_user}" "${forever_logfile}"
+
+  [ "${forever_outfile}" != "" ] && \
+    /usr/bin/touch "${forever_outfile}" && \
+    /usr/sbin/chown "${forever_user}" "${forever_outfile}"
+
+  [ "${forever_errfile}" != "" ] && \
+    /usr/bin/touch "${forever_errfile}" && \
+    /usr/sbin/chown "${forever_user}" "${forever_errfile}"
+
+  [ "${forever_pid}" != "" ] && \
+    /usr/bin/touch "${forever_pid}" && \
+    /usr/sbin/chown "${forever_user}" "${forever_pid}"
+
+  # Make the forever_root if it doesn't exist.
+  if [ ! -d "${forever_root}" ]
+  then
+    log "Making \"${forever_root}\""
+    mkdir -p mkdir -p "${forever_root}"
+  fi
+
+  /usr/sbin/chown -R "${forever_user}" "${forever_root}"
+
 }
 
 run_rc_command "$1"
